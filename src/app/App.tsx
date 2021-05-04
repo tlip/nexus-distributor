d10597a7ea94346b6121808766c8f510db274616
tlip <thomas.lipari@gmail.com>
2021-05-03 17:28:40 -0700

    convert App content to React Router switches

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,40 @@
 import React from 'react';
+import styled from '@emotion/styled/macro';
+import { HashRouter, BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'theme-ui';
+
 import { Flex } from 'components/Flex';
-import { Card } from 'components/Card';
-import { Text } from 'components/Text';
-import { Button } from 'components/Button';
 import { NavBar } from 'components/NavBar';
+import { PageContentWrapper } from 'components/PageContentWrapper';
+import { OpportunityListView } from 'views/OpportunityListView';
+
 import { theme } from 'theme';
+import { env } from 'config/env';
+
+const AppContainer = styled(Flex)((props: any) => ({
+  justifyContent: 'center',
+  width: 'clamp(100vw, 100%, 100vw)',
+  minHeight: 'calc(100vh - 2.75em)',
+  background: props.theme.colors.background,
+  transform: 'translateY(2.75em)',
+  padding: '2.75em 0',
+}));
+
+const Router = env.GITHUB_PAGES
+  ? (props: any) => <HashRouter {...props} />
+  : (props: any) => <BrowserRouter {...props} />;
 
 export const App: React.FC = () => (
   <ThemeProvider theme={theme}>
-    <NavBar />
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      width="100vw"
-      minHeight="100vh"
-      height="100vh"
-      bg="background"
-    >
-      <Card width="100%" maxWidth="20em">
-        <Text variant="h4">Hello, welcome to Nexus Mutual</Text>
-        <Text as="p" mt="2em">
-          I think i'm just text. That's pretty cool, right?
-        </Text>
-        <Text as="div" mt="1em" mb="2em">
-          Test my buttons!
-        </Text>
-        <Button width="100%" my="0.5em">
-          Primary Button
-        </Button>
-        <Button variant="outline" width="100%">
-          Outline Button
-        </Button>
-      </Card>
-    </Flex>
+    <Router>
+      <NavBar />
+      <AppContainer>
+        <PageContentWrapper>
+          <Switch>
+            <Route path="/" component={OpportunityListView} />
+          </Switch>
+        </PageContentWrapper>
+      </AppContainer>
+    </Router>
   </ThemeProvider>
 );
